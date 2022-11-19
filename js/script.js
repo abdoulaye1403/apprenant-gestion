a8796652e7b9ac1f224eb66f6891f09ee8b20f7f
abdoulaye <abdelraul140385@gmail.com>
2022-11-19 22:15:09 +0000

    code final

--- js/script.js
+++ js/script.js
@@ -1,116 +1,151 @@
+var form = document.getElementById('form')
+var tableBody = document.querySelector('.table tbody')
+let submitbtn = document.getElementById('submitbtn')
 
+let editMode = false
+let editionApprenant = null
 
- // class apprenant 
+editModeEnabled(editMode)
+
 class Apprenants{
-    constructor(id,nom,postnom,prenom,pays,genre,github){
-        this.id = id
-        this.nom = nom;
-        this.postnom = postnom;
-        this.prenom = prenom;
-        this.pays = pays;
-        this.genre = genre;
-        this.github = github
+         constructor(id,nom,postnom,prenom,pays,genre,github){
+             this.id = id
+             this.nom = nom;
+             this.postnom = postnom;
+             this.prenom = prenom;
+             this.pays = pays;
+             this.genre = genre;
+             this.github = github
+         }
+        
+     }
+var apprenant1 = new Apprenants(1,'Toure','Mahamadou','Abdoulaye','Mali','Masculin','abdelraul@github.com');
+var apprenant2 = new Apprenants(2,'Maiga','Houney','Aicha','Mali','Feminin','aicha@github.com')
+var apprenants = [apprenant1,apprenant2]
+
+function loadApprenantInTable() {
+    tableBody.innerHTML = ''
+
+    for (var apprenant of apprenants) {
+        let temp = `<tr>
+            <td>${apprenant.id}</td>
+            <td>${apprenant.nom}</td>
+            <td>${apprenant.postnom}</td>
+            <td>${apprenant.prenom}</td>
+            <td>${apprenant.pays}</td>
+            <td>${apprenant.genre}</td>
+            <td>${apprenant.github}</td>
+            <td>
+                <button  onclick="deleteApprenant(this)" class="btn btn-danger">Supprimmer</button>
+            </td>
+            <td>
+                <button data-nom="${apprenant.nom}" data-id="${apprenant.id}"  
+                data-postnom="${apprenant.postnom}"  data-prenom="${apprenant.prenom}"  data-pays="${apprenant.pays}"  data-genre="${apprenant.genre}"
+                data-github="${apprenant.github}"  onclick="editApprenant(this)"  class="btn btn-warning"
+                >
+                Modifier</button>
+            </td>
+        </tr>`
+
+        tableBody.innerHTML += temp
+    }
+
+    
+}
+
+loadApprenantInTable()
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    var idValue = document.querySelector('#id').value;
+     var nomValue = document.querySelector('#nom').value;
+     var postnomValue = document.querySelector('#postnom').value;
+     var prenomValue = document.querySelector('#prenom').value;
+     var paysValue = document.querySelector('#pays').value;
+     var genreValue = document.querySelector('#genre').value;
+     var githubValue = document.querySelector('#github').value;
+     if(idValue==='' || nomValue==='' || postnomValue==='' || prenomValue==='' || paysValue==='' || genreValue==='' || githubValue===''){
+
+                  alert("Tous les champs doivent etre rempli")
+                
+             }else{
+        
+                     
+                 var apprenant = new Apprenants(idValue,nomValue,postnomValue,prenomValue,paysValue,genreValue,githubValue);
+        
+               
+        
+                 if(editMode) {
+                    updateApprenant(apprenant)
+                } else {
+                    addApprenant(apprenant);
+                }
+                    
+        
+                     
+                     viderChamp();
+                
+                 }
+})
+
+
+    
+    
+function viderChamp(){
+              document.querySelector('#id').value = '';
+              document.querySelector('#nom').value = '';
+              document.querySelector('#postnom').value = '';
+              document.querySelector('#prenom').value = '';
+              document.querySelector('#pays').value = '';
+              document.querySelector('#genre').value = '';
+              document.querySelector('#github').value = '';
+}
+
+function updateApprenant(apprenant) {
+    apprenants.find((t) => t.id == editionApprenant.id).nom = apprenant.nom
+    apprenants.find((t) => t.id == editionApprenant.id).postnom = apprenant.postnom
+    apprenants.find((t) => t.id == editionApprenant.id).prenom = apprenant.prenom
+    apprenants.find((t) => t.id == editionApprenant.id).pays = apprenant.pays
+    apprenants.find((t) => t.id == editionApprenant.id).genre = apprenant.genre
+    apprenants.find((t) => t.id == editionApprenant.id).github = apprenant.github
+    loadApprenantInTable()
+
+      // init
+     editModeEnabled(false)
+ }
+
+function addApprenant(apprenant) {
+     apprenants.push(apprenant)
+     loadApprenantInTable()
     }
     
+
+
+
+function deleteApprenant(e){
+    e.parentNode.parentNode.remove()
 }
 
- // class interface apprenant
-class ApprenantInterface{
-    
-    // methode ajouter un apprenant
-    static addInListApprenant(apprenant){
-        var tableBody = document.querySelector('.table tbody')
-        tableBody.innerHTML = ''
-            var temp = `<tr>
-                <td>${apprenant.id}</td>
-                <td>${apprenant.nom}</td>
-                <td>${apprenant.postnom}</td>
-                <td>${apprenant.prenom}</td>
-                <td>${apprenant.pays}</td>
-                <td>${apprenant.genre}</td>
-                <td>${apprenant.github}</td>
-                <td>
-                    <button class = "btn btn-danger">Supprimmer</button>
-                </td>
-                <td>
-                    <button class="btn btn-warning">Modifier</button>
-                </td>
-            </tr>`
-    
-            tableBody.innerHTML += temp        
-            
-        }
-
-// // methode afficher message d'erreur
-    static alertMessage(message){
-        form.insertAdjacentHTML('beforebegin',`<div class = "alert alert-info">${message}</div>`)
-        var alert = document.querySelector(".alert");
-        var liste = document.querySelector('.liste')
-        setTimeout(()=>{
-            liste.removeChild(alert);
-        }, 2000);
-        }
-    
-        // methode vider les champs
-        static viderChamp(){
-             document.querySelector('#id').value = '';
-             document.querySelector('#nom').value = '';
-             document.querySelector('#postnom').value = '';
-             document.querySelector('#prenom').value = '';
-             document.querySelector('#pays').value = '';
-             document.querySelector('#genre').value = '';
-             document.querySelector('#github').value = '';
-        
-        }
+function editApprenant(e) {
+    editModeEnabled(true)
+    document.querySelector('#nom').value = e.dataset.nom
+    document.querySelector('#postnom').value = e.dataset.postnom
+    document.querySelector('#prenom').value = e.dataset.prenom
+    document.querySelector('#pays').value = e.dataset.pays
+    document.querySelector('#genre').value = e.dataset.genre
+    document.querySelector('#github').value = e.dataset.github
+    editionApprenant = apprenants.find((t) => t.id == e.dataset.id)
 }
 
-  document.addEventListener('DOMContentLoaded',ApprenantInterface.ApprenantList);
-
-// // ajouter un apprenant
-var form = document.querySelector("#form");
-form.addEventListener('click',function(e){
-    // empecher une erreur
-    e.preventDefault()
-
-    // recuperer les champs
-    var idValue = document.querySelector('#id').value;
-    var nomValue = document.querySelector('#nom').value;
-    var postnomValue = document.querySelector('#postnom').value;
-    var prenomValue = document.querySelector('#prenom').value;
-    var paysValue = document.querySelector('#pays').value;
-    var genreValue = document.querySelector('#genre').value;
-    var githubValue = document.querySelector('#github').value;
-
-    // verifier si les champs ne sont pas vide
-
-    if(idValue==='' || nomValue==='' || postnomValue==='' || prenomValue==='' || paysValue==='' || genreValue==='' || githubValue===''){
-
-        // afficher le message d'erreur
-       ApprenantInterface.alertMessage("Tous les champs doivent etre rempli")
-    }else{
-
-        //    creer un apprenant
-        var apprenant = new Apprenants(idValue,nomValue,postnomValue,prenomValue,paysValue,genreValue,githubValue);
-
-       
-
-        //    ajouter un apprenant dans la liste
-           ApprenantInterface.addInListApprenant(apprenant)
-
-        //    vider les champs
-           ApprenantInterface.viderChamp();
-        
-        }
-    })
-
-    var tableBody = document.querySelector(".table tbody")
-    tableBody.addEventListener('click',(e)=>{
-        e.preventDefault();
-        var tr = e.target.parentElement.parentElement;
-        tableBody.removeChild(tr)
-        
-    })
-
-
-
+function editModeEnabled(enabled) {
+    if(enabled) {
+        editMode = true
+        submitbtn.innerText = "Modifier"
+    } else {
+        editMode = false
+        submitbtn.innerText = "Ajouter"
+        editionApprenant = null
+        viderChamp()
+    }
+}
     
